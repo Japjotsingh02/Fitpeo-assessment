7d26f4973a663edd00bde31a00bd3409f93d6043
Japjotsingh02 <sjapjot2002@gmail.com>
2023-02-28 13:05:15 +0530

    assessment completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='flex'>
+        <Sidebar/>
+        <Routes>
+          <Route path='/' exact element={<Dashboard/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
